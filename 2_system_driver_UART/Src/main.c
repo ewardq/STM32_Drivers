632a821f7601be29ea0ccddf74bc59369fbc2a2d
Eduardo Martínez Piña <72580785+ewardq@users.noreply.github.com>
2025-02-19 11:33:26 -0600

    Add UART driver files

---
+++ 2_system_driver_UART/Src/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stm32f4xx.h>
+#include "fpu.h"
+#include "uart.h"
+/* Module:
+ * FPU
+ * UART
+ * GPIO
+ * TIMEBASE
+ */
+
+#define GPIOAEN  (1U<<0)  // Bit number zero is set to 1
+#define PIN5     (1U<<5)
+#define LED_PIN  PIN5     // Give it another name so it's more readable
+
+/*
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+*/
+
+int main(void)
+{
+    /* Enable FPU */
+	fpu_enable();
+
+	/*Initialize UART*/
+	debug_uart_init();
+
+	while(1){
+		printf("Hello from STM32 \n\r");
+		for(int i = 0; i < 9000; i++){};
+	}
+}
